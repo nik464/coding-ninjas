1f927b506d48b9299c8e6d991e4ab8c2a180f0a2
Nikhil Chaudhary <nikhil18chaudhary@gmail.com>
2021-09-09 21:50:03 +0530

    min steps

---
+++ dynamic programming/1_minsteps.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << countMinStepsToOne(n);
+}
+
+int countMinStepsToOne(int n)
+{
+	//Write your code here
+    	if(n <= 1) {
+		return 0;
+	}
+
+	// Recursive call
+	int x = countMinStepsToOne(n - 1);
+
+	int y = INT_MAX, z = INT_MAX;
+	if(n % 2 == 0) {
+		y = countMinStepsToOne(n/2);
+	}
+
+	if(n % 3 == 0) {
+		z = countMinStepsToOne(n/3);
+	}
+
+	// Calculate final output
+	int ans = min(x, min(y, z)) + 1;
+
+	return ans;
+}
