7d94a0dc8d8808e0f5d16bc5ea9efd8d7eaee729
Nikhil Chaudhary <nikhil18chaudhary@gmail.com>
2021-09-09 22:13:27 +0530

    staircase

---
+++ dynamic programming/3_staircase.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << staircase(n);
+}
+long staircase(int n)
+{
+	//Write your code here
+        
+    long dp[n + 1];
+    
+    dp[0]=1;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i = 3; i <= n; i++){
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    
+    return dp[n];
+    
+}
