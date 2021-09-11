593caa88ac0cf87b8b77b4f86d8cc9d63b3ea9d6
Nikhil Chaudhary <nikhil18chaudhary@gmail.com>
2021-09-11 12:04:16 +0530

    ksmallest

---
+++ priority queue/4_ksmallest.cpp
@@ -0,0 +1,57 @@
+#include <queue>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+    int size;
+    cin >> size;
+
+    int *input = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> input[i];
+    }
+
+    int k;
+    cin >> k;
+
+    vector<int> output = kSmallest(input, size, k);
+    sort(output.begin(), output.end());
+
+    for (int i = 0; i < output.size(); i++) {
+        cout << output[i] << " ";
+    }
+
+    delete[] input;
+}
+
+vector<int> kSmallest(int arr[], int n, int k) {
+    // Write your code here
+    priority_queue<int> pq;
+    
+    for(int i=0;i<k;i++)
+        pq.push(arr[i]);
+    
+    for(int i=k;i<n;i++)
+    {
+        if(pq.top()>arr[i])
+        {pq.pop();
+         
+         pq.push(arr[i]);
+        }
+    }
+    
+     vector<int> ans;
+    
+    while(!pq.empty())
+    {
+      ans.push_back(pq.top());
+        pq.pop();
+        
+    }
+    return ans;
+}
